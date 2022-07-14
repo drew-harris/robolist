95503fdd561279e09c38b80d5c66913014f76137
Drew Harris <dsharris10@gmail.com>
2022-07-13 20:50:35 -0500

    Fix nextconfig

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function Web() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Web</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>increase</button>
     </div>
   );
 }
