098cd1fad9f4acf8c771001a9671cd9658e9f83f
Drew Harris <dsharris10@gmail.com>
2022-07-14 23:52:08 -0500

    Middleware test

---
+++ apps/web/pages/api/protected/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req?.method != "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  // Begins here
+  return res.status(200).json({
+    message: "This route is protected by a JWT token.",
+  });
+}
