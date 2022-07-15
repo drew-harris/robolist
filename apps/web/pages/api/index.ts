49a997301753979f78341b4e8d2ca0ac306360df
Drew Harris <dsharris10@gmail.com>
2022-07-14 23:30:27 -0500

    Force "GET" method for hello world

--- apps/web/pages/api/index.ts
+++ apps/web/pages/api/index.ts
@@ -4,12 +4,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req?.method != "POST") {
+  if (req?.method != "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
   // Begins here
   return res.status(200).json({
-    message: "Hello world",
+    message: "Robolist: The easiest way to get work done",
   });
 }
