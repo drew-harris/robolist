51966cb0d3a73e4b88d3ec37b1b09c27cc3b6961
Drew Harris <dsharris10@gmail.com>
2022-07-14 23:28:30 -0500

    Fix incorrect method in hello world

--- apps/web/pages/api/index.ts
+++ apps/web/pages/api/index.ts
@@ -5,11 +5,11 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req?.method != "POST") {
-    res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
   // Begins here
-  res.status(200).json({
+  return res.status(200).json({
     message: "Hello world",
   });
 }
