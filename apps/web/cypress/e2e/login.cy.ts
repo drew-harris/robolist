41402c44a934c12954d2f0822dc686c6fbbf77c0
Drew Harris <dsharris10@gmail.com>
2022-07-20 23:58:56 -0500

    Add class tests

--- apps/web/cypress/e2e/login.cy.ts
+++ apps/web/cypress/e2e/login.cy.ts
@@ -1,3 +1,4 @@
+import user from "../fixtures/login-details.json";
 describe("Logs in", () => {
   it("passes", () => {
     cy.visit("http://localhost:3000/");
@@ -20,8 +21,19 @@ describe("Logs in", () => {
     const createAccountButton = cy.get(".mantine-1nwce10 > .mantine-Text-root");
     createAccountButton.should("exist");
     createAccountButton.click();
-
     cy.url().should("include", "signup");
+  });
+
+  it("should navigate back to login page", () => {
+    cy.visit("http://localhost:3000/login");
+  });
+
+  it("should login", () => {
+    cy.get(".mantine-TextInput-root").type(user.email);
+    cy.get(".mantine-PasswordInput-root").type(user.password);
+    cy.get(".mantine-Button-filled").click();
+    cy.contains("Today");
+    cy.contains("All Tasks");
   });
 });
 
--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
+
+import user from "../fixtures/login-details.json";
+
+import cypress from "cypress";
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -25,15 +30,17 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(): Chainable<void>;
+      drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>;
+      dismiss(
+        subject: string,
+        options?: Partial<TypeOptions>
+      ): Chainable<Element>;
+    }
+  }
+}
 
 export {};
