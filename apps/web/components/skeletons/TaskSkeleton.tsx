5adaae05390a3d9c11a577b40a3ece577e2b1d9e
Drew Harris <dsharris10@gmail.com>
2022-08-14 12:03:38 -0500

    Change to grid daily tasks + analytics

--- apps/web/components/skeletons/TaskSkeleton.tsx
+++ apps/web/components/skeletons/TaskSkeleton.tsx
@@ -3,17 +3,13 @@ import React from "react";
 import { TaskOptionProps } from "../data-display/Task";
 
 const TaskSkeleton = ({ checkbox = false, ...props }: TaskOptionProps) => {
-	const [fakeTitle] = React.useState(
-		"a".repeat(Math.floor(Math.random() * 10) + 8)
-	);
-
 	// random width between 100 and 200
 	return (
 		<Paper p="md" shadow="xs">
 			<Group>
 				{checkbox && <Skeleton width={20} height={20} />}
 				<Box>
-					<Skeleton visible={true}>{fakeTitle}</Skeleton>
+					<Skeleton visible={true}>AAAAAAAAAAAA</Skeleton>
 				</Box>
 			</Group>
 		</Paper>
