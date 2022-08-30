510688da95774b8e5adc507aa35fe124e9629eae
Drew Harris <dsharris10@gmail.com>
2022-08-29 19:51:47 -0500

    Add class for skeleton

--- apps/web/pages/classes/index.tsx
+++ apps/web/pages/classes/index.tsx
@@ -4,21 +4,60 @@ import { useMediaQuery } from "@mantine/hooks";
 import { Class } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import ClassSquare from "../../components/data-display/ClassSquare";
+import ClassSkeleton from "../../components/skeletons/ClassSkeleton";
 import CenterInfo from "../../components/small/CenterInfo";
 import NewClassButton from "../../components/small/NewClassButton";
 import useInitialPrefetch from "../../hooks/useInitialPrefetch";
-import { vanilla } from "../../utils/trpc";
+import useSkeletonCount from "../../hooks/useSkeletonCount";
+import { InferQueryOutput, vanilla } from "../../utils/trpc";
 
-const ClassesPage = () => {
+interface ClassesContainerProps {
+	classes: InferQueryOutput<"classes.all"> | undefined;
+	loading: boolean;
+	skeletonLength?: number;
+}
+
+const ClassesContainer = ({
+	classes,
+	loading,
+	skeletonLength = 4,
+}: ClassesContainerProps) => {
 	const [parent] = useAutoAnimate<HTMLDivElement>();
 	const isMobile = useMediaQuery("(max-width: 900px)", false);
+	if (loading) {
+		return (
+			<>
+				<SimpleGrid cols={isMobile ? 1 : 3}>
+					{[...Array(skeletonLength)].map((e, i) => (
+						<ClassSkeleton key={i} />
+					))}
+				</SimpleGrid>
+			</>
+		);
+	}
+	return (
+		<>
+			<SimpleGrid ref={parent} cols={isMobile ? 1 : 3}>
+				{classes?.map((class_) => (
+					<ClassSquare key={class_.id} class={class_} />
+				))}
+			</SimpleGrid>
+		</>
+	);
+};
+
+const ClassesPage = () => {
 	const {
 		data: classes,
 		error,
 		status,
-	} = useQuery<Class[], Error>(["classes"], () => vanilla.query("classes.all"));
+	} = useQuery<InferQueryOutput<"classes.all">, Error>(["classes"], () =>
+		vanilla.query("classes.all")
+	);
 
 	useInitialPrefetch();
+
+	const skeletonCount = useSkeletonCount("classes", classes);
 
 	const classElements = classes
 		? classes.map((class_) => {
@@ -41,15 +80,12 @@ const ClassesPage = () => {
 			</Box>
 
 			{error && <CenterInfo color="red" text={error.message} />}
-			{status === "loading" && (
-				<Center>
-					<Loader />
-				</Center>
-			)}
 			{classes && classes.length == 0 && <CenterInfo text="No classes yet" />}
-			<SimpleGrid ref={parent} cols={isMobile ? 1 : 3}>
-				{classElements}
-			</SimpleGrid>
+			<ClassesContainer
+				skeletonLength={skeletonCount}
+				classes={classes}
+				loading={status === "loading"}
+			/>
 		</>
 	);
 };
